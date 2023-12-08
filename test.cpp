7400d60f02114d3e2ccbdcc3f4e069259d6b425b
zhuangyunchi <zhuangyunchi@gmail.com>
2023-12-08 21:32:01 +0800

    cout sth

--- test.cpp
+++ test.cpp
@@ -6,6 +6,6 @@ int main()
     cout << "hello world" << endl;
     cout << "wtf" << endl;
     cout << "hi" << endl;
-    
+    cout << "zhuangyunchi dont know how to use git" << endl; 
     return 0;
 }

00e1fc1725e29c47fc41cbaf3a3f6c0889117abf
Toad_S <toadshih650@gmail.com>
2023-12-11 13:15:23 +0800

    cover test

--- test.cpp
+++ test.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
+#include <unistd.h>
 using namespace std;
 
 int main()
 {
     cout << "hello world" << endl;
-    cout << "wtf" << endl;
-    cout << "hi" << endl;
+    usleep(3000000);
+    system("clear");
     return 0;
 }

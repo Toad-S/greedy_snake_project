cc2f678e3ec73d42a9343a1598cd298e54c708f0
Toad_S <toadshih650@gmail.com>
2023-12-14 09:04:37 +0800

    snake can move smoothly with 3 length

--- main.cpp
+++ main.cpp
@@ -4,6 +4,8 @@
 #include <vector>
 #include <deque>
 #include <cstdlib>
+#include <ctime>
+#include <list>
 #include "get_keyboard_input.cpp"
 //因為不知道要怎麼一起執行 main.cpp 和 get_keyboard_input.cpp 所以先 include .cpp file instead of .hpp file
 using namespace std;
@@ -15,8 +17,12 @@ const int MAP_VERBOND = 4; // The size of the left and right boundary (2 + 2)
 struct Snake
 {
     int len; // The snake's  length
-    int x; // The x coordinate of the snake
-    int y; // The y coordinate of the snake
+    int x; // The x coordinate of the snake head
+    int y; // The y coordinate of the snake head
+    // Every int vector in the deque is a coordinate of one part of the snake body
+    // The first element in the deque is the head of the snake
+    // The last element in the deque is the tail of the snake
+    deque<vector<int> > body;
 };
 
 struct Map
@@ -71,11 +77,11 @@ int main()
     // Get the map vector
     mapData.map = createMap(mapData);
 
-    // Print the map
-    printCurMap(mapData);
-
     // Create the snake
     createsnake(mapData, snake);
+
+    // Print the map
+    printCurMap(mapData);
 
     char pre_key = 'd'; // 預設如果沒按按鍵就先往右走
 
@@ -125,96 +131,191 @@ int main()
 //testing function
 void createsnake(Map& mapData, Snake& snake)
 {
-    // Choose the random size
-
     //The four corner of the map
     int top = MAP_HORBOND / 2;
+    int bottom = mapData.map_verlen + (MAP_HORBOND / 2) - 1;
     int leftMost = MAP_VERBOND / 2;
 
-    // Create x to imitate the snake
-    snake.x = leftMost;
-    snake.y = top;
-    mapData.map[snake.y][snake.x] = 'x'; // Set x at the top leftMost at first
+    // Choose the random place where the snake would be generated
+    // The random place would only be on four boundaries
+    srand(time(0));
+    //randomSide = 0 -> on the top boundary
+    //randomSide = 1 -> on the bottom boundary
+    //randomSide = 2 -> on the left boundary
+    int randomSide = rand() % 3;
+
+    vector<int> coordinate(2);
+
+    switch (randomSide)
+    {
+        case 0:
+            {
+                int randomPlace0 = rand() % (mapData.map_horlen - 5);
+                
+                snake.y = top;
+                snake.x = leftMost + randomPlace0;
+
+                // Push the x, y to the vector (head)
+                coordinate[0] = snake.x;
+                coordinate[1] = snake.y;
+                // Push the coordinate to the deque
+                snake.body.push_back(coordinate);
+
+                // Push the x, y to the vector
+                coordinate[0] = snake.x - 1;
+                coordinate[1] = snake.y;
+                // Push the coordinate to the deque
+                snake.body.push_back(coordinate);
+
+                // Push the x, y to the vector (tail)
+                coordinate[0] = snake.x - 2;
+                coordinate[1] = snake.y;
+                // Push the coordinate to the deque
+                snake.body.push_back(coordinate);
+            }
+
+            break;
+        case 1:
+            {
+                int randomPlace1 = rand() % (mapData.map_horlen - 5);
+                
+                snake.y = bottom;
+                snake.x = leftMost + randomPlace1;
+
+                // Push the x, y to the vector (head)
+                coordinate[0] = snake.x;
+                coordinate[1] = snake.y;
+                // Push the coordinate to the deque
+                snake.body.push_back(coordinate);
+
+                // Push the x, y to the vector
+                coordinate[0] = snake.x - 1;
+                coordinate[1] = snake.y;
+                // Push the coordinate to the deque
+                snake.body.push_back(coordinate);
+
+                // Push the x, y to the vector (tail)
+                coordinate[0] = snake.x - 2;
+                coordinate[1] = snake.y;
+                // Push the coordinate to the deque
+                snake.body.push_back(coordinate);
+            }
+
+            break;
+        case 2:
+            {
+                int randomPlace2 = rand() % (mapData.map_verlen - 2);
+
+                snake.y = top + randomPlace2;
+                snake.x = leftMost + 2;
+
+                // Push the x, y to the vector (head)
+                coordinate[0] = snake.x;
+                coordinate[1] = snake.y;
+                // Push the coordinate to the deque
+                snake.body.push_back(coordinate);
+
+                // Push the x, y to the vector
+                coordinate[0] = snake.x - 1;
+                coordinate[1] = snake.y;
+                // Push the coordinate to the deque
+                snake.body.push_back(coordinate);
+
+                // Push the x, y to the vector (tail)
+                coordinate[0] = snake.x - 2;
+                coordinate[1] = snake.y;
+                // Push the coordinate to the deque
+                snake.body.push_back(coordinate);
+            }
+
+            break;
+    }
+
+    // Set the snake on the map
+    // snake.body[0] 存了 snake head 的 vector 座標，snake.body[0][0] 是 x 座標 snake.body[0][1] 是 y 座標
+    // snake.body[1] 存了 snake body 的 vector 座標，snake.body[0][0] 是 x 座標 snake.body[0][1] 是 y 座標
+    // snake.body[2] 存了 snake tail 的 vector 座標，snake.body[0][0] 是 x 座標 snake.body[0][1] 是 y 座標
+    mapData.map[snake.body[0][1]][snake.body[0][0]] = '>';
+    mapData.map[snake.body[1][1]][snake.body[1][0]] = 'x';
+    mapData.map[snake.body[2][1]][snake.body[2][0]] = 'x';
 }
 
 //testing function
 void snakeMove(char& key, Map& mapData, Snake& snake, char& pre_key)
 {
-    //The four corner of the map
+    // The four corner of the map
     int top = MAP_HORBOND / 2;
     int bottom = mapData.map_verlen + (MAP_HORBOND / 2) - 1;
     int leftMost = MAP_VERBOND / 2;
     int rightMost = mapData.map_horlen + (MAP_VERBOND / 2) - 1;
 
+    // Pop the snake tail
+    vector<int> tail = snake.body.back();
+    mapData.map[tail[1]][tail[0]] = ' '; // tail[1] is the y coordinate of the snake tail; tail[0] is the x coordinate of the snake tail
+    snake.body.pop_back();
+
+    // Modify the snake head before the move into a part of snake body
+    vector<int> oldhead = snake.body.front();
+    mapData.map[oldhead[1]][oldhead[0]] = 'x'; // oldhead[1] is the y coordinate of the old snake head; oldhead[0] is the x coordinate of the odl snake head
+
+    // New head
+    char head_appearence; // Snake head's appearence would change by direction
+
+    // Decide the snake head's next position, and set snake.x and snake.y to the position
+    // Decide the snake head's new appearence
     switch(key)
         {
             case 'w':
-                // When pressing valid control button, make the snake's old place = ' '
-                mapData.map[snake.y][snake.x] = ' ';
-
                 // Snake move
                 if (snake.y == top) // Snake penetrate through the top boundary
                 {
                     snake.y = bottom;
-                    mapData.map[snake.y][snake.x] = 'x';
                 }
                 else
                 {
                     snake.y--;
-                    mapData.map[snake.y][snake.x] = 'x';
                 }
 
+                head_appearence = '^';
                 break;
             case 's':
-                // When pressing valid control button, make the snake's old place = ' '
-                mapData.map[snake.y][snake.x] = ' ';
-
                 // Snake move
                 if (snake.y == bottom) // Snake penetrate through the bottom boundary
                 {
                     snake.y = top;
-                    mapData.map[snake.y][snake.x] = 'x';
                 }
                 else
                 {
                     snake.y++;
-                    mapData.map[snake.y][snake.x] = 'x';
                 }
 
+                head_appearence = 'v';
                 break;
             case 'a':
-                // When pressing valid control button, make the snake's old place = ' '
-                mapData.map[snake.y][snake.x] = ' ';
-
                 // Snake move
                 if (snake.x == leftMost) // Snake penetrate through the left boundary
                 {
                     snake.x = rightMost;
-                    mapData.map[snake.y][snake.x] = 'x';
                 }
                 else
                 {
                     snake.x--;
-                    mapData.map[snake.y][snake.x] = 'x';
                 }
 
+                head_appearence = '<';
                 break;
             case 'd':
-                // When pressing valid control button, make the snake's old place = ' '
-                mapData.map[snake.y][snake.x] = ' ';
-
                 // Snake move
                 if (snake.x == rightMost) // Snake penetrate through the right boundary
                 {
                     snake.x = leftMost;
-                    mapData.map[snake.y][snake.x] = 'x';
                 }
                 else
                 {
                     snake.x++;
-                    mapData.map[snake.y][snake.x] = 'x';
                 }
 
+                head_appearence = '>';
                 break;
             case '0':
                 // User doesn't input, so snake moves with the same direction
@@ -222,77 +323,67 @@ void snakeMove(char& key, Map& mapData, Snake& snake, char& pre_key)
                 switch(pre_key)
                 {
                     case 'w':
-                        // When pressing valid control button, make the snake's old place = ' '
-                        mapData.map[snake.y][snake.x] = ' ';
-
                         // Snake move
                         if (snake.y == top) // Snake penetrate through the top boundary
                         {
                             snake.y = bottom;
-                            mapData.map[snake.y][snake.x] = 'x';
                         }
                         else
                         {
                             snake.y--;
-                            mapData.map[snake.y][snake.x] = 'x';
                         }
 
+                        head_appearence = '^';
                         break;
                     case 's':
-                        // When pressing valid control button, make the snake's old place = ' '
-                        mapData.map[snake.y][snake.x] = ' ';
-
                         // Snake move
                         if (snake.y == bottom) // Snake penetrate through the bottom boundary
                         {
                             snake.y = top;
-                            mapData.map[snake.y][snake.x] = 'x';
                         }
                         else
                         {
                             snake.y++;
-                            mapData.map[snake.y][snake.x] = 'x';
                         }
 
+                        head_appearence = 'v';
                         break;
                     case 'a':
-                        // When pressing valid control button, make the snake's old place = ' '
-                        mapData.map[snake.y][snake.x] = ' ';
-
                         // Snake move
                         if (snake.x == leftMost) // Snake penetrate through the left boundary
                         {
                             snake.x = rightMost;
-                            mapData.map[snake.y][snake.x] = 'x';
                         }
                         else
                         {
                             snake.x--;
-                            mapData.map[snake.y][snake.x] = 'x';
                         }
 
+                        head_appearence = '<';
                         break;
                     case 'd':
-                        // When pressing valid control button, make the snake's old place = ' '
-                        mapData.map[snake.y][snake.x] = ' ';
-
                         // Snake move
                         if (snake.x == rightMost) // Snake penetrate through the right boundary
                         {
                             snake.x = leftMost;
-                            mapData.map[snake.y][snake.x] = 'x';
                         }
                         else
                         {
                             snake.x++;
-                            mapData.map[snake.y][snake.x] = 'x';
                         }
 
+                        head_appearence = '>';
                         break;
                 }
-
                 break;
         }
+
+    // Adding new head
+    vector<int> newhead(2);
+    newhead[0] = snake.x;
+    newhead[1] = snake.y;
+    mapData.map[newhead[1]][newhead[0]] = head_appearence;
+    snake.body.push_front(newhead);
 }
 
 vector<vector<char> > createMap(Map& mapData)

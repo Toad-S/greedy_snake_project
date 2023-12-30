957109e9e04f9d346f1b9eb92add8dbb15369f69
Toad_S <toadshih650@gmail.com>
2023-12-30 20:51:56 +0800

    all comment added

--- main.cpp
+++ main.cpp
@@ -22,59 +22,156 @@ struct Snake
     int len; // The snake's  length
     int x; // The x coordinate of the snake head
     int y; // The y coordinate of the snake head
+
     // Every int vector in the deque is a coordinate of one part of the snake body
     // The first element in the deque is the head of the snake
     // The last element in the deque is the tail of the snake
     deque<vector<int> > body;
-    int starve_timer;
+    int starve_timer; // 紀錄蛇連續沒吃到食物的秒數，蛇一吃到食物，starve_timer就會歸零重新開始計算，若連續大於 15 秒沒吃東西，蛇就會死掉
 };
 
 struct Map
 {
+    // 以二維 vector 的形式存 map，map 包括整張地圖、地圖邊界、每個當下的蛇、每個當下的食物、障礙物
+    // map會隨著遊戲進行而改變，所有遊戲的動態變動（計分板以外）都會由 map 顯示
     vector<vector<char> > map;
     int map_verlen; // Map vertical length
     int map_horlen; // Map horizontal length
-    int refresh_frequency; // 控制 getkeyin() 的鍵盤偵測時間（因為鍵盤偵測時間 > usleep()時間，所以地圖更新速度受鍵盤偵測時間影響）
-    int gametimer;
+
+    // refresh_frequency 控制接收鍵盤按鍵輸入的函式 getkeyin() 等待鍵盤輸入的時間
+    // main function 中的第二層 while loop 每跑一圈就會印出 map 一次（視為地圖動態更新）
+    // 而第二層 while loop 中有 getkeyin() 和 usleep() 兩個 function 會造成 while loop 的延遲
+    // 其中 usleep() 是為了不造成電腦過大的負擔，所以每一圈 loop 都要讓程式停一下
+    // 而 getkeyin() 則是接收鍵盤輸入的 function，其運作方式會先等待鍵盤輸入一段時間，若使用者在那段時間內有任何鍵盤輸入，就會接收鍵盤輸入，若使用者在那段時間內沒有鍵盤輸入，函式則會回傳 0(type: char)
+    // 由於等待鍵盤輸入的時間至少是 usleep() 的 45 倍以上，所以控制地圖更新速度主要是控制 getkeyin() 中等待鍵盤輸入的時間
+    // 因此將此變數命名為 refresh_frequency
+    int refresh_frequency;
+    int gametimer; // 紀錄每一局遊戲的持續時間
 };
 
 struct Food
 {
-    vector<vector<int> > normal_coordinate;
-    vector<vector<int> > poison_coordinate;
+    vector<vector<int> > normal_coordinate; // 存所有普通食物的座標
+    vector<vector<int> > poison_coordinate; // 存所有陷阱食物的座標
+
+    // 紀錄食物的存在時間，普通食物出現三秒後會改變自己的外觀，提示玩家食物快要消失了，但陷阱食物不會改變外觀
+    // 所有食物出現六秒後會重生在不同位置
     int timer;
 };
 
 void snakeMove(char&, Map&, Snake&, char&, int&);
+// Input argument 1: key, 是來自 getkeyin() 的 return 值，且在 main function 中被篩選過，只有 'w', 'a', 's', 'd', '0' 會進入 snakeMove 中（其中，當使用者沒輸入時，getkeyin()會 return '0'）
+// Input argument 2: mapData, 調用 Map structure 中的所有資料
+// Input argument 3: snake, 調用 Snake structure 中的所有資料
+// Input argument 4: pre_key, pre_key 為上一個使用者輸入，用來阻擋不合理的移動和判斷使用者沒輸入(key == '0')時，蛇要繼續往哪個方向走
+// Input argument 5: state, 來自 checkCollision() 的 return 值，表示蛇在移動中遇到的三種狀況：
+// Input argument 5: state == 0 代表蛇正常移動，也就是蛇頭往移動方向多一格，且去掉蛇尾巴，使蛇長度維持
+// Input argument 5: state == 1 代表蛇變長一單位，也就是蛇頭往移動方向多一格，但不去尾
+// Input argument 5: state == 2 代表蛇撞到障礙物。但因為撞到障礙物就會死亡，所以 state == 2 的狀況會在 main function 中被過濾掉，不會進入 snakeMove() 中，因此 snakeMove() 沒有 state == 2 的判斷
+// Input argument 5: state == 0 1 2 以外的數 代表蛇撞到自己的身體
+// Function 1: 當 state == 0 時，蛇會正常按照鍵盤輸入(key == 'w', 'a', 's', 'd')方向轉向移動，若鍵盤沒有輸入(key == '0')，則會維持原本的移動方向
+// Function 2: 當蛇移動到邊界時則會穿牆
+// Function 3: 當 state == 1 時，蛇會變長一單位，並且讓 snake.len++
+// Function 4: 當 state == n(n 是0 1 2 以外的數)，代表蛇撞到自己的第 state 節身體，此時會先去除蛇的撞到的那節以後的身體，再讓蛇正常移動
 
 vector<vector<char> > createMap(Map&);
-// Intput argument 1: map_verlen, the gaming map's vertical length 
-// Input argument 2: map_horlen, the gaming map's horizontal length
-// map_verlen and map_horlen can be input by user before the game starts
-// The function creates and return a map vector, which is constructed by a 2D vector
+// Intput argument 1: mapData, 調用 Map 中的 map_horlen 和 map_verlen 
+// Intput argument 1: map_verlen and map_horlen can be input by user before the game starts
+// Return 1: createMap() 回傳一個按照使用者輸入的地圖大小建立好的二維 vector，當作整張地圖
+// Function 1: 二維 vector 中指包含了地圖的邊界，其餘中心部分則都是由空白組成的(key spaces)
+// Function 1: 而回傳的二維空地圖 vector 則會由 main function 中的二維 vector 接收，並作為往後遊戲進行時的地圖，任何操作的結果都會顯示在地圖上
 
 void createsnake(Map&, Snake&);
+// Input argument 1: mapData, 調用 Map 中的 map_verlen，和 const MAP_HORBOND 和 const MAP_VERBOND 定出蛇隨機生成的範圍
+// Input argument 2: snake, 調用 Snake 中的 body(type: deque<vector<int> >) 。在隨機生成蛇之後，要把蛇的每一節身體座標放入 body 中
+// Function 1: 確立蛇隨機生成的範圍（地圖的上邊界、下邊界、左邊界，由於蛇生在右邊界，一出生就會穿牆，並從左邊界出現，所以不特別讓蛇隨機出生在右邊界）
+// Function 2: 在隨機生成範圍中隨機生成蛇的頭的座標，並由頭座標決定每節身體座標
+// Function 3: 將每節身體和頭座標都放入 body 中，並於地圖對應座標上印出蛇的頭和身體
 
 void recordboard(Map&, Snake&);
-// The function prints score and rules
+// Input argument 1: mapData, 調用 Map 中的 gametimer 和 refresh_frequency
+// Input argument 2: snake, 調用 Snake 中的 len 和 starve_timer
+// Function 1: record board 永遠會被印在地圖上方，且包含以下資訊：
+// 1. 遊戲規則
+// 2. 目前分數（蛇長度）
+// 3. 遊戲進行時間（單位：秒）
+// 4. 蛇的飢餓時間（單位：秒）
+// 5. 螢幕刷新速度（意義是螢幕刷新週期）（單位：微秒/次），刷新週期越短代表蛇會動得越快
 
 void printCurMap(Map&);
+// Input argument 1: mapData, 調用 Map 中的 map (type: vector<vector<char> >)、map_horlen、map_horlen
+// Function 1: 以 Map 中的 map (type: vector<vector<char> >)、map_horlen、map_verlen 結合 const MAP_HORBOND 和 const MAP_VERBOND 印出地圖
 
 void createBarrier(Map&, vector<vector<int> >&);
+// Input argument 1: mapData, 調用 Map 中的 map_horlen、map_verlen
+// Input argument 2: vector<vector<int> > barriers 儲存所有障礙物座標的 vector
+// Function 1: 在地圖中匡限一塊能生成障礙物的區塊，並將該區塊切分成一快快相鄰的 4x4 小區塊，每個小區塊都是障礙物能隨機生成的位置
+// Function 2: 隨機在小區塊中生成 n 塊障礙物(n 是按照障礙物和地圖面積的比例決定)
+// Function 2: 生成障礙物時，會另外隨機決定障礙物外型（將從預先設定好的八種障礙物排列中挑出一種）作為該障礙物外型（由 selectBarrier() 負責）
+// Function 2: 將小區塊的最左上座標存入 vector<int> referencePoint 中，並將 referencePoint 傳入 selectBarrier() 中，決定障礙物外型
 
 void selectBarrier(Map&, vector<int>&, vector<vector<int> >&);
+// Input argument 1: mapData, 調用 Map 中的 map，將障礙物印在地圖上
+// Input argument 2: referencePoint, 來自 createBarrier()，傳遞 4x4 小區塊中的左上角座標，作為隨機生成障礙物的座標依據
+// Input argument 3: barriers, 儲存障礙物座標的二維 vector
+// Function 1: 根據 createBarrier() 傳來的 referencePoint，建立選定小區塊中的隨機障礙物外型
+// Function 2: 依照隨機外型和 referencePoint 決定障礙物座標，並印在地圖上及存入 vector<vector<int> >barriers 中
 
 void generateFood(Map&, Food&, Snake&);
+// Input argument 1: mapData, 調用 Map 中的 map、map_horlen、map_verlen
+// Input argument 2: food, 調用 Food 中的 normal_coordinate、poison_coordinate、timer
+// Input argument 3: snake, 調用 Snake 中的 x, y
+// Function 1: 以照地圖面積(map_horlen * map_verlen) 決定要生成的食物數量 n 個
+// Function 2: 在地圖內隨機生成 n 個食物
+// Function 2: 並確認隨機生成的食物座標不在障礙物上或是蛇的目前位置上，否則重新生成隨機食物座標，直到確認食物座標不會和其他物件重疊為止
+// Function 2: 在 n 個食物中有 10% 的食物會是陷阱食物
+// Function 3: 將食物座標按照食物類別分別存入 vector<vector<int> > normal_coordinate 和 vector<vector<int> > poison_coordinate
+// Function 4: 生成完食物後，將計算食物存在地圖時間的 timer 歸零
 
 void clearFood(Map&, Food&);
+// Input argument 1: mapData, 調用 Map 中的 map
+// Input argument 2: food, 調用 Food 中的 normal_coordinate、poison_coordinate
+// Function 1: 在地圖上將所有食物置換成空白(keyboard space)
+// Function 2: 將 normal_coordinate、poison_coordinate 兩 vector 清空
 
 void foodRot(Map&, Food&);
+// Input argument 1: mapData, 調用 Map 中的 map
+// Input argument 2: food, 調用 Food 中的 normal_coordinate
+// Function 1: 將普通食物的外型從 '+' 改成 '&' 以提示玩家食物快要過期了
 
 void speedUp(Map&, int);
+// Input argument 1: mapData, 調用 Map 中的 refresh_frequency
+// Input argument 2: mode, 可為 0 或 1（為了增加程式本身的可擴充性，不排除可增加其他模式，所以用 int 而非 bool）
+// Function 1: mode == 0: 遊戲每進行 15 秒，就讓蛇的速度變快一點（refresh_frequency -= 1000 將地圖重置週期降低 1000 微秒）（較無明顯感覺）
+// Function 2: mode == 1: 一旦蛇吃到陷阱食物，蛇的移動速度就會大幅增加（refresh_frequency -= 10000 將地圖重置週期降低  10000 微秒）（有感）
+// Function 3: 不管是 mode == 0 或 1，只要 refresh_frequency 為 45000，就不會再降低（refresh_frequency 低於 45000 微秒會造成螢幕閃爍）
 
 int checkCollision(Map&, Snake&, char&, char&);
+// Input argument 1: mapData, 調用 Map 中的 map、map_horlen、map_verlen、body
+// Input argument 2: snake, 調用 Snake 中的 x、y
+// Input argument 3: key, 鍵盤當下的輸入（key 只可能 ==  '0', 'w', 'a', 's', 'd'）
+// Input argument 4: pre_key, 鍵盤前一次輸入
+// Function 1: 按照鍵盤輸入算出蛇頭的下一個位置
+// Function 1: 如果前一次鍵盤輸入和當下鍵盤輸入在同一方向軸上（例如前一次輸入'w'，當下輸's'或'w'），就將 key 從原本的方向改成 '0'（視同沒有改變移動方向）
+// Function 1: 蛇到邊界會穿牆
+// Function 2: 判斷蛇頭的下一個位置的座標對應到地圖上是什麼字元
+// Function 2: 如果是 ' '，代表蛇頭沒有撞到東西，就 return state = 0（代表正常移動）
+// Function 2: 如果是 '+' 或 '&'，都代表蛇吃到普通食物，就 return state = 1（代表蛇吃到東西並正常移動）
+// Function 2: 如果是 '#'，代表蛇吃到陷阱食物，就先呼叫 speed(mapData, 1) 並 return state = 1
+// Function 2: 如果是 'x'，代表蛇撞到自己的身體，則計算蛇撞到第幾節身體，並 return state = 撞到第幾節身體（撞到第五節身體就 return 5）
+// Function 2: 如果是 'H'，代表蛇撞到障礙物，則 return 2（代表蛇撞到障礙物）
 
 int gameOver(Map&, Snake&, int);
+// Input argument 1: mapData, 調用 Map 中的 gametimer
+// Input argument 2: snake, 調用 Snake 中的 len
+// Input argument 3: deadmessage, 代表遊戲結束原因（死亡原因）
+// Function 1: 從 gameover.txt 中印出 Game over 字樣
+// Function 2: 由 len 印出玩家該局得分
+// Function 3: 由 gametimer 印出玩家該局遊玩時間
+// Function 4: 由 deadmessage 印出遊戲結束原因
+// Function 4: deadmessage == 1 代表玩家因撞到牆而使遊戲結束
+// Function 4: deadmessage == 2 代表玩家因餓死(15秒內沒吃到食物)而使遊戲結束
+// Function 4: deadmessage == 3 代表玩家按了 'r' 使遊戲重新開始新的一局
 
 
 int main()
@@ -1151,7 +1248,7 @@ void foodRot(Map& mapData, Food& food)
 
 void speedUp(Map& mapData, int mode)
 {
-    if ((mapData.refresh_frequency >= 45000) && (mode == 0)) // mode 0 是每aaasaaaawddsaaawdwad 15 秒蛇自動加速一次的加速速率
+    if ((mapData.refresh_frequency >= 45000) && (mode == 0)) // mode 0 是每 15 秒蛇自動加速一次的加速速率
     {
         mapData.refresh_frequency -= 1000;
     }
